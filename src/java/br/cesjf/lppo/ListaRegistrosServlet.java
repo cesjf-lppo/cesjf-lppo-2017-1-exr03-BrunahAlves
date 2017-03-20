09fff70cbae06db0e7771e28d1a8edc88f0487c1
BrunahAlves <brunahalves94@gmail.com>
2017-03-20 00:21:31 -0300

    Cria lista-registros e ListaRegistrosServlet

---
+++ src/java/br/cesjf/lppo/ListaRegistrosServlet.java
@@ -0,0 +1,73 @@
+package br.cesjf.lppo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Bruna Alves
+ */
+@WebServlet(name = "ListaRegistrosServlet", urlPatterns = {"/Lista.html"})
+public class ListaRegistrosServlet extends HttpServlet {
+
+    
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List <Visitante> visitantes = new ArrayList<>();
+        
+                try {
+
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conexao = DriverManager.getConnection("jdbc:derby://localhost:1527/lppo-2017-1", "usuario", "senha");
+            Statement operacao = conexao.createStatement();
+            ResultSet resultado = operacao.executeQuery("SELECT * FROM visitante");
+            while(resultado.next()){
+                Visitante visitante = new Visitante();
+                visitante.setId(resultado.getLong("Id"));
+                visitante.setNome(resultado.getString("nome"));
+                visitante.setIdade(resultado.getInt("idade"));
+                visitante.setEntrada(resultado.getString("entrada"));
+                visitante.setSaida(resultado.getString("saida"));
+                visitantes.add(visitante);
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ListaRegistrosServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaRegistrosServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        request.setAttribute("visitantes", visitantes);
+        request.getRequestDispatcher("WEB-INF/lista-registros.jsp").forward(request, response);
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/WEB-INF/lista-registros.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : lista-registros
+    Created on : 20/03/2017, 00:12:24
+    Author     : Bruna Alves
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>Data de entrada</th>
+                <th>Data de saída</th>
+            </tr>
+            <c:forEach var="visitante" items="${visitantes}">
+                <tr>
+                    <td><a href="edita.html?id=${visitante.id}">${visitante.id}</a></td>
+                    <td>${visitante.nome}</td>
+                    <td>${visitante.idade}</td>
+                    <td>${visitante.entrada}</td>
+                    <td>${visitante.saida}</td>
+                    <td><a href="exclui.html?id=${pessoa.id}">X</a></td>
+                </tr>
+            </c:forEach>
+        </table>
+    </body>
+</html>
