4b771722fd74f37a345dd7257c984f3b587646c3
BrunahAlves <brunahalves94@gmail.com>
2017-03-19 23:53:41 -0300

    Cria Visitante

---
+++ src/java/br/cesjf/lppo/Visitante.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cesjf.lppo;
+
+/**
+ *
+ * @author Bruna Alves
+ */
+public class Visitante {
+    private Long id;
+    private String nome;
+    private int idade;
+    private String entrada;
+    private String saida;
+    
+    public Visitante(){
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the entrada
+     */
+    public String getEntrada() {
+        return entrada;
+    }
+
+    /**
+     * @param entrada the entrada to set
+     */
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+
+    /**
+     * @return the saida
+     */
+    public String getSaida() {
+        return saida;
+    }
+
+    /**
+     * @param saida the saida to set
+     */
+    public void setSaida(String saida) {
+        this.saida = saida;
+    }
+    
+    
+    
+}
