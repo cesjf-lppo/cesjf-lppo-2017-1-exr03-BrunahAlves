e02bf3650834857a64519029c481b479cb15b0f4
BrunahAlves <brunahalves94@gmail.com>
2017-03-24 21:47:04 -0300

    Cria SaidaRegistroServlet

---
+++ src/java/br/cesjf/lppo/SaidaRegistroServlet.java
@@ -0,0 +1,47 @@
+package br.cesjf.lppo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SaidaRegistroServlet", urlPatterns = {"/saida.html"})
+public class SaidaRegistroServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Long id = Long.parseLong(request.getParameter("id"));
+
+        try {
+
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conexao = DriverManager.getConnection("jdbc:derby://localhost:1527/lppo-2017-1", "usuario", "senha");
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate("UPDATE visitante SET saida=CURRENT_TIMESTAMP WHERE id=" + id);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ListaRegistrosServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaRegistrosServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        response.sendRedirect("lista.html");
+
+    }
+
+}
